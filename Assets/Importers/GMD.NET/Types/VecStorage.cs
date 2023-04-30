39c38efed2c54cbf1ed68e7c1fcfcae0e7e493da
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-30 19:02:52 +0100

    Parse vertex format bitfield

---
+++ Assets/Importers/GMD.NET/Types/VecStorage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.Rendering;
+
+public struct VecStorage {
+    VertexAttributeFormat Component;
+    int NumComponents;
+
+    public VecStorage(VertexAttributeFormat component, int numComponents) {
+        if (numComponents < 2 || numComponents > 4) {
+            throw new System.ArgumentOutOfRangeException("numComponents " + numComponents + " not between 2 and 4");
+        }
+
+        Component = component;
+        NumComponents = numComponents;
+    }
+}
